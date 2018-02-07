a30ee49378fe1705769bee748f36e5611bb00fec
Ricardo Riveron <larius828@utexas.edu>
2018-02-07 02:31:23 -0600

    Added skeleton webpages. Initial index.php and about.php

---
+++ index.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    	<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/bootstrap-grid.min.css">
+		<link rel="stylesheet" href="css/bootstrap-reboot.min.css">
+
+		<link rel="icon" size="96x96" href="imgs/favicon.png">
+
+		<title>On The Run</title>
+	</head>
+	<body>
+		<div id="wrapper">
+	    	<nav class="navbar sticky-top navbar-expand-md navbar-dark bg-primary">
+			  	<a class="navbar-brand mb-0 h1" href="#">
+			  		<img src="imgs/favicon.png" width="30" height="30" class="d-inline-block align-top" alt="">
+			  		OnTheRun
+			  	</a>
+			 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+				</button>
+
+			  	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			    	<ul class="navbar-nav mr-auto">
+			      		<li class="nav-item active">
+			        		<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+			      		</li>
+			      		<li class="nav-item">
+					        <a class="nav-link" href="about.php">About</a>
+					    </li>
+					</ul>
+					<form class="form-inline my-2 my-lg-0">
+					    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+					    <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
+					</form>
+			  	</div>
+			</nav>
+			<div id="page-wrapper">
+				<div class="container-fluid">
+					<br>
+					<!-- Page Heading -->
+					<div class="row">
+						<div class="col-lg-4">
+							<div class="card text-center" style="width: 18rem;">
+								<img class="card-img-top" src="imgs/mug_example.jpg" alt="Card image cap">
+								<div class="card-body">
+									<h5 class="card-title">Mugshot Example</h5>
+									<h6 class="card-subtitle mb-2 text-muted">Crime listed here</h6>
+									<p class="card-text">Here we could put data about the criminal. Blah, blah, blah...</p>
+									<div class="btn-group" role="group">
+										<a href="#" class="btn btn-primary btn-sm">Link To State</a>
+										<a href="#" class="btn btn-primary btn-sm">Link To Type of Crime</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="card text-center" style="width: 18rem;">
+								<img class="card-img-top" src="imgs/prison_example.jpg" alt="Card image cap">
+								<div class="card-body">
+									<h5 class="card-title">State Example</h5>
+									<h6 class="card-subtitle mb-2 text-muted">State/Country listed here</h6>
+									<p class="card-text">Here we could put data about the state. Blah, blah, blah...</p>
+									<div class="btn-group" role="group">
+										<a href="#" class="btn btn-primary btn-sm">Link To Criminals</a>
+										<a href="#" class="btn btn-primary btn-sm">Link To Crimes</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="card text-center" style="width: 18rem;">
+								<img class="card-img-top" src="imgs/crime_example.jpg" alt="Card image cap">
+								<div class="card-body">
+									<h5 class="card-title">Crime Example</h5>
+									<p class="card-text">Here we could put data about the crime category. Blah, blah, blah...</p>
+									<div class="btn-group" role="group">
+										<a href="#" class="btn btn-primary btn-sm">Link To Criminals</a>
+										<a href="#" class="btn btn-primary btn-sm">Link To State</a>
+									</div>
+								</div>
+							</div>
+						</div>						
+					</div>
+				</div>
+			</div>
+		</div>
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>

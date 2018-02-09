be1095534e67f6c70537ef6ed41fdbf1eb6df6cb
Ricardo Riveron <larius828@yahoo.com>
2018-02-09 16:27:06 -0600

    Added all the pathing and created structure to handle .html's for our models

--- static_new/app.py
+++ static_new/app.py
@@ -6,13 +6,37 @@ app = Flask(__name__)
 
 # use decorators to link the function to a url
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')  
 
 @app.route('/about')
-def welcome():
+def about():
     return render_template('about.html') 
+
+@app.route('/crime')
+def crime():
+    return render_template('crime.html') 
+
+@app.route('/criminal')
+def criminal():
+    return render_template('criminal.html')
+
+@app.route('/state')
+def state():
+    return render_template('state.html') 
+
+@app.route('/crime1')
+def crime1():
+    return render_template('crime1.html')
+
+@app.route('/criminal1')
+def criminal1():
+    return render_template('criminal1.html')
+
+@app.route('/state1')
+def state1():
+    return render_template('state1.html')    
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=80)

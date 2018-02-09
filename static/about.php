af0bd060dca1bc6d84c233d430c6a634f59e8d9e
Roshan Dongre <rdongre@utexas.edu>
2018-02-08 18:34:59 -0600

    Added static folder

---
+++ static/about.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    	<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/bootstrap-grid.min.css">
+		<link rel="stylesheet" href="css/bootstrap-reboot.min.css">
+
+		<link rel="icon" size="96x96" href="imgs/favicon.png">
+
+		<title>On The Run</title>
+	</head>
+	<body>
+		<div id="wrapper">
+	    	<nav class="navbar sticky-top navbar-expand-md navbar-dark bg-primary">
+			  	<a class="navbar-brand mb-0 h1" href="index.php">
+			  		<img src="imgs/favicon.png" width="30" height="30" class="d-inline-block align-top" alt="">
+			  		OnTheRun
+			  	</a>
+			 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+				</button>
+
+			  	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			    	<ul class="navbar-nav mr-auto">
+			      		<li class="nav-item">
+			        		<a class="nav-link" href="index.php">Home</a>
+			      		</li>
+			      		<li class="nav-item active">
+					        <a class="nav-link" href="#">About <span class="sr-only">(current)</span></a>
+					    </li>
+					</ul>
+					<form class="form-inline my-2 my-lg-0">
+					    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+					    <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
+					</form>
+			  	</div>
+			</nav>
+			<div id="page-wrapper">
+				<div class="container-fluid">
+					<br>
+					<h1>This is the criteria we have to meet here:</h1>
+					<td>
+						<p>
+							<b>one</b> page
+						</p>
+						<ul>
+							<li>description of the site, its purpose, its intended users</li>
+							<li>explanation of the interesting result of integrating disparate data</li>
+							<li>the group name</li>
+							<li>the group members</li>
+							<li>for each member
+							<ul>
+								<li>name</li>
+								<li>photo</li>
+								<li>bio</li>
+								<li>major responsibilities</li>
+								<li>no. of commits</li>
+								<li>no. of issues</li>
+								<li>no. of unit tests</li>
+							</ul>
+							</li>
+							<li>stats
+							<ul>
+								<li>total no. of commits</li>
+								<li>total no. of issues</li>
+								<li>total no. of unit tests</li>
+							</ul>
+							</li>
+							<li>data
+							<ul>
+								<li>links to the data <b>sources</b></li>
+								<li>description of <b>how</b> each was <b>scraped</b></li>
+							</ul>
+							</li>
+							<li>tools
+							<ul>
+								<li>tools used</li>
+								<li>describe their use</li>
+								<li>special focus on optional tools that were <b>not</b> required</li>
+							</ul>
+							</li>
+							<li>a link to the <b>GitBook</b> report</li>
+							<li>a link to the <b>GitHub</b> repo</li>
+						</ul>
+					</td>
+				</div>
+			</div>
+		</div>
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>

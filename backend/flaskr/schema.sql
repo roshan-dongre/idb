89906665466a2855c2f759aed1d3096775bc42ea
Ricardo Riveron <larius828@yahoo.com>
2018-03-07 16:42:42 +0000

    Adding skeleton for backend, need to change schema

---
+++ backend/flaskr/schema.sql
@@ -0,0 +1,7 @@
+
+p table if exists entries;
+create table entries (
+  id integer primary key autoincrement,
+  title text not null,
+  'text' text not null
+);`
---
+++ backend/setup.py
@@ -0,0 +1,11 @@
+
+m setuptools import setup
+
+setup(
+    name='flaskr',
+    packages=['flaskr'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)

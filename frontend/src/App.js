77280c8a09e938159144c15961202c994fc51a9a
Zara Louis <zaralouis13@gmail.com>
2018-03-07 17:12:55 -0600

    Updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import Header from './Header'
+import Main from './Main'
+import 'react-bootstrap';
 
-export default App;
+const App = () => (
+  <div>
+    <Header />
+    <Main />
+  </div>
+)
+
+export default App  

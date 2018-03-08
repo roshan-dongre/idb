520dbbedf6b144fdb71c128d5623c60467729035
Roshan Dongre <rdongre@utexas.edu>
2018-03-07 23:38:22 -0600

    got json parsing somewhat working

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Carousel, Container, Slide} from 'react-bootstrap'
+import $ from 'jquery'
 
 var imageStyles = {
     height: "637.5px",
@@ -15,22 +16,46 @@ var headerStyle={
     opacity: '0.0',
 }
 
-
 class Home extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { person: []};
+  }
+
+
+  fetchFirst() {
+      var that = this;
+      if (true) {
+        fetch('http://ontherun.me:5000/api').then(function (response) {
+          console.log(response.json());
+          return response.json();
+        });
+      }
+    }
+
+    componentWillMount() {
+        this.fetchFirst();
+    }
+
     render() {
         return (
             <div>
-            
                 <Carousel controls={false}>
                     <Carousel.Item>
+                      <Carousel.Caption>
                         <img className="center-block" alt="900x500"
                              src="http://prospect.rsc.org/blogs/cw/wp-content/uploads/2009/11/crimescene-tape-4559193-ji.jpg"
                              style={imageStyles}/>
+                      </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item animateIn>
+                      <Carousel.Caption>
                         <img className="center-block" alt="900x500"
                              src="http://projects.aljazeera.com/2014/chicago-homicides/images/chicago_homicide_01_crime_scene3.jpg"
                              style={imageStyles}/>
+                        <h3> {this.state.person} </h3>
+                      </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img className="center-block" alt="900x500"
@@ -41,7 +66,7 @@ class Home extends React.Component {
 
                 <div>
             <h1 style={headerStyle}><b>On The Run</b></h1>
-                    
+
                 </div>
             </div>
         )

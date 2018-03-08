259094ba0648d27f31d1a050469ff76552efe96f
Ricardo Riveron <larius828@yahoo.com>
2018-03-08 04:01:01 +0000

    Created .db file and set up schema for future entries

--- backend/flaskr/flaskr.py
+++ backend/flaskr/flaskr.py
@@ -1,4 +1,4 @@
-mport os
+import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
@@ -20,3 +20,34 @@ def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print('Initialized the database.')
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+# start the server with the 'run()' method
+if __name__ == '__main__':
+    initdb_command()
+    app.run(host='0.0.0.0', port=5000)

afb28198f05ceca1c7c13c7c0bcd96840153682e
Roshan Dongre <rdongre@utexas.edu>
2018-03-14 15:11:51 -0500

    more of about page done

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -76,7 +76,7 @@ render () {
                                 <img className="img-thumbnail" src="https://rp27537cs373sp18.files.wordpress.com/2018/01/why.png" alt="Audric Bitmoji" data-toggle="modal" data-target="#audricModal" style = {imageStyles}/>
                                 <h2>Ramon Perez</h2>
                                 <p>Bio: CS student hailing from New Braunfels, Texas. This is my third year as part of UTCS, and I'm in the Software Engineering and Artificial Intelligence classes this semester. I enjoy video games, running, and playing with my cats.</p>
-                                <p>Responsibilities: Management of the data, including scraping, aggregating, and data modelling.</p>
+                                <p>Responsibilities: Data Management: scraping, aggregating, and data modelling.</p>
                                 <p>Number of commits: TBA</p>
                                 <p>Number of issues: TBA</p>
                                 <p>Number of unit tests: 0</p>
@@ -88,7 +88,7 @@ render () {
                                 <img className="img-thumbnail" src="https://ramdeepk2.github.io/ramdeepk2.github.io/cs373/headshot.JPG" alt="Pablo Bitmoji" data-toggle="modal" data-target="#pabloModal" style = {imageStyles}/>
                                 <h2>Krishna Ramdeep</h2>
                                 <p>Bio: CS senior taking Computer Vision, Programming for Performance, Software Engineering, and Randomized Algorithms. I like to work out, hike, and read in my free time (which I don't get too much of).</p>
-                                <p>Responsibilities: Design 'about' page, Integrate GitHub dynamic data, document API on GitBook, write technical report on Gitbook</p>
+                                <p>Responsibilities: API Report</p>
                                 <p>Number of commits: TBA</p>
                                 <p>Number of issues: TBA</p>
                                 <p>Number of unit tests: 0</p>
@@ -102,38 +102,46 @@ render () {
                                         <h1><u>Stats</u></h1>
                                         <ul>
                                             <li className="text-left"><h4><strong>total no. of commits:</strong></h4></li>
-                                            <li className="text-left"><h4><strong>total no. of issues:</strong> 126</h4></li>
-                                            <li className="text-left"><h4><strong>total no. of unit tests:</strong> 146</h4></li>
-                                            <li className="text-left"><a href="http://docs.crafthats.apiary.io/"><h4>Apiary API</h4></a></li>
-                                            <li className="text-left"><a href="https://github.com/RJMathis/crafthats"><h4>GitHub Repo</h4></a></li>
-                                            <li className="text-left"><a href="https://trello.com/b/gV83PBgA/swe-project-1"><h4>Trello</h4></a></li>
+                                            <li className="text-left"><h4><strong>total no. of issues:</strong> TBA</h4></li>
+                                            <li className="text-left"><h4><strong>total no. of unit tests:</strong> TBA</h4></li>
                                         </ul>
                                     </div>
                                 </div>
-                                <div className="col-md-3 container data-thumbnail">
+                                <div className="col-md-2 container data-thumbnail">
                                     <div className="text-center">
                                         <h1><u>Tools</u></h1>
                                         <ul>
-                                            <li className="text-left"><h4>PlanIt Poker</h4></li>
                                             <li className="text-left"><h4>Slack</h4></li>
-                                            <li className="text-left"><h4>Google Hangouts</h4></li>
+                                            <li className="text-left"><h4>Flask</h4></li>
+                                            <li className="text-left"><h4>React</h4></li>
+                                            <li className="text-left"><h4>Postman</h4></li>
+                                            <li className="text-left"><h4>Travis CI</h4></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div className="col-md-2 container data-thumbnail">
+                                    <div className="text-center">
+                                        <h1><u>Tools</u></h1>
+                                        <ul>
+                                            <li className="text-left"><h4>GitHub</h4></li>
+                                            <li className="text-left"><h4>Mocha</h4></li>
+                                            <li className="text-left"><h4>Selenium</h4></li>
                                         </ul>
                                     </div>
                                 </div>
                                 <div className="col-md-3 container data-thumbnail">
                                     <div className="text-center">
                                         <h1><u>Data</u></h1>
-                                        <p><strong>Brewery DB:</strong> Used API to collect data via HTTP GET requests</p>
+                                        <p><strong>FBI Crime Stats API:</strong> Provided crime stats through GET Requests</p>
                                         <p><strong>RateBeer:</strong>   Used API to collect data via HTTP POST requests</p>
                                     </div>
                                 </div>
-                                <div className="col-md-3 container data-thumbnail">
+                                <div className="col-md-2 container data-thumbnail">
                                     <div className="text-center">
                                         <h1><u>Technical</u></h1>
-                                        <a href="https://travisreed7.gitbooks.io/technical-documentation/content/"><h4>Technical Report</h4></a>
-                                        <a href="https://yuml.me/12ee86c4.pdf"><h4>UML Diagram</h4></a>
-                                        <a href="http://aganser.com/visualization.html"><h4>Visualization</h4></a>
-                                        <a href="https://gitpitch.com/RJMathis/crafthats/gitPitch"><h4>Presentation</h4></a>
+                                        <a href="https://roshan-dongre.gitbooks.io/api/"><h4>API Documentation</h4></a>
+                                        <a href="https://roshan-dongre.gitbooks.io/report/"><h4>Technical Report</h4></a>
+                                        <a href="https://github.com/roshan-dongre/idb"><h4>GitHub Repository</h4></a>
                                     </div>
                                 </div>
                             </div>

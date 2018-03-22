60f6cadcfd66ef6303d891bf9773ad36449a874f
Ricardo Riveron <larius828@yahoo.com>
2018-03-21 22:15:25 -0500

    Fixed typo on repr function

--- backend/flaskr/flaskr.py
+++ backend/flaskr/flaskr.py
@@ -9,26 +9,9 @@ import json
 
 app = Flask(__name__) # create the application instance :)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////mydb.db' # load config from this file , flaskr.py
-app.config['SERVER_NAME'] = 'ontherun.me:5000'
+# app.config['SERVER_NAME'] = 'ontherun.me:5000'
 CORS(app)
 db = SQLAlchemy(app)
-
-statelist = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID",
-    "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT",
-    "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
-    "SD", "TN", "TX", "UT", "VT", "VA", "WA", "DC", "WV", "WI", "WY"]
-    
-poplist = {"AL": 4863300, "AK": 741894, "AZ": 6931071, "AR": 2988248, "CA": 39250017,
-    "CO": 5540545, "CT": 3576452, "DE": 952065, "FL": 20612439, "GA": 10310371,
-    "HI": 1428557, "ID": 1683140, "IL": 12801539, "IN": 6633053, "IA": 3134693,
-    "KS": 2907289, "KY": 4436974, "LA": 4681666, "ME": 1331479, "MD": 6016447,
-    "MA": 6811779, "MI": 9928300, "MN": 5519952, "MS": 2988726, "MO": 6093000,
-    "MT": 1042520, "NE": 1907116, "NV": 2940058, "NH": 1334795, "NJ": 8944469,
-    "NM": 2081015, "NY": 19745289, "NC": 10146788, "ND": 757952, "OH": 11614373,
-    "OK": 3923561, "OR": 4093465, "PA": 12784227, "RI": 1056426, "SC": 4961119,
-    "SD": 865454, "TN": 6651194, "TX": 27862596, "UT": 3051217, "VT": 624594,
-    "VA": 8411808, "WA": 7288000, "DC": 681170, "WV": 1831102, "WI": 5778708,
-    "WY": 585501, "": 323127513}
 
 class State(db.Model):
     id = db.Column(db.Integer,primary_key=True,unique=True)
@@ -51,9 +34,9 @@ class Criminal(db.Model):
     race = db.Column(db.String(80))
     nationality = db.Column(db.String(80))
     crime = db.Column(db.String(600), nullable=False)
-    image = db.Column(db.String(150))
+    image = db.Column(db.String(600))
     def __repr__(self):
-        return "{'image': %r,id': %r, 'name': %r, 'field_office': %r, 'height': %r, 'weight': %r, 'sex': %r, 'hair': %r, 'eyes': %r, 'dob': %r, 'race': %r, 'nationality': %r, 'crime': %r}" % (self.image, self.id, self.name, self.field_office, self.height, self.weight, self.sex, self.hair, self.eyes, self.dob, self.race, self.nationality, self.crime)
+        return "{'image': %r, 'id': %r, 'name': %r, 'field_office': %r, 'height': %r, 'weight': %r, 'sex': %r, 'hair': %r, 'eyes': %r, 'dob': %r, 'race': %r, 'nationality': %r, 'crime': %r}" % (self.image, self.id, self.name, self.field_office, self.height, self.weight, self.sex, self.hair, self.eyes, self.dob, self.race, self.nationality, self.crime)
 
 
 class Crime(db.Model):
@@ -125,7 +108,7 @@ if __name__ == '__main__':
         NewRace = person["race_raw"]
         NewNationality = person["nationality"]
         NewCrime = person["caution"]
-        NewImage = person["images"][0]["large"]
+        NewImage = person["images"]
         NewCriminal = Criminal(name=NewName,
                               field_office=NewFieldOffice[0],
                               height=NewHeight,
@@ -137,7 +120,7 @@ if __name__ == '__main__':
                               race=NewRace,
                               nationality=NewNationality,
                               crime=NewCrime,
-                              image=NewImage)
+                              image=NewImage[0]["large"])
         db.session.add(NewCriminal)
 
     with open('../crime_data/crime_ids.txt') as fp:

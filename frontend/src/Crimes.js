6e258093e0e93389c7aea3987e8bde8df6011060
Zara Louis <zaralouis13@gmail.com>
2018-03-21 22:10:10 -0500

    Crimes grid

--- frontend/src/Crimes.js
+++ frontend/src/Crimes.js
@@ -35,7 +35,6 @@ export default class Crimes extends Component {
 
     componentDidMount () {
         this.callAPI()
-        this.getStatesCriminals()
     }
 
     handlePageChange = (page, e) => {
@@ -58,18 +57,6 @@ export default class Crimes extends Component {
         }
     }
 
-    handleCriminal = (e) => {
-        this.setState({selectedCriminal: e.target.value})
-    }
-
-    handleState = (e) => {
-        this.setState({selectedState: e.target.value})
-    }
-
-    sort = (order) => {
-        this.setState({sortBy: order})
-    }
-
     callAPI = () => {
 
         // let limit = this.state.pageLimit
@@ -78,48 +65,18 @@ export default class Crimes extends Component {
         
         let url = "http://api.ontherun.me:5000/crimes"
 
-        if (this.state.selectedState !== "All" && this.state.selectedState !== "") {
-            url += "&state="+this.state.selectedState
-        }
-        if (this.state.selectedCriminal !== "All" && this.state.selectedCriminal !== "") {
-            url += "&criminal="+this.state.selectedCriminal
-        }
-        if (this.state.sortBy !== "") {
-            url += "&sort_by="+this.state.sortBy
-        }
-
         let self = this
         axios.get(url)
             .then((res) => {
                 // Set state with result
-                self.setState({crimes: res.data.records, totalCount: res.data.totalCount, numPages: Math.ceil(res.data.totalCount/self.state.pageLimit)});
+                self.setState({crimes: res.data.crimes, totalCount: res.data.crimes.length, numPages: Math.ceil(res.data.totalCount/self.state.pgSize)});
             })
             .catch((error) => {
                 console.log(error)
             });
     }
 
-    getStatesCriminals = () => {
-        let url = 'http://api.ontherun.me:5000/crimes';
-        let states = new Set([])
-        let criminals = new Set([])
-
-        let self = this
-        axios.get(url)
-            .then((res) => {
-                // Set state with names of styles
-                res.data.records.forEach((crime) => {
-                    states.add(crime.state);
-                    criminals.add(crime.criminal)
-                })
-                states = ['All',...Array.from(states).sort()]
-                criminals = ['All',...Array.from(criminals).sort()]
-                self.setState({allStates: states, allCriminals: criminals});
-            })
-            .catch((error) => {
-                console.log(error)
-            });
-    }
+    
 
     /* Updating
         An update can be caused by changes to props or state. These methods are called when a component is being re-rendered:
@@ -156,63 +113,28 @@ export default class Crimes extends Component {
 
     /* More information about the React.Component lifecyle here: https://reactjs.org/docs/react-component.html */
     render () {
-        let stateMenu = []
-        let criminalMenu = []
-
+     
+        let styleMenu = []
+        let crimeComponents = []
         // Create an array of X components with 1 for each crime gathered from API call
-        let crimeComponents = this.state.crimes.map(function(crime) {
+        crimeComponents = this.state.crimes.map(function(crime) {
                 return (
                     <ItemSelector item={crime} navigateTo="/Crimes"/>
                 );
             })
 
-        if (this.state.allCriminals !== undefined) {
-            stateMenu = this.state.allStates.map((criminal) => {
+        if (this.state.crimes !== undefined) {
+            // Create an array of X components with 1 for each beer gathered from API call
+            crimeComponents = this.state.crimes.map((crime) => {
                 return (
-                    <option value={criminal}>{criminal}</option>
-                );
-            })
-        }
-
-        if (this.state.allStates !== undefined) {
-            criminalMenu = this.state.allCriminals.map((state) => {
-                return (
-                    <option value={state}>{state}</option>
+                    <ItemSelector item={crime} navigateTo="/Crimes"/>
                 );
             })
         }
 
         return (
             <div className="container sub-container">
-                <div className="row">
-                    <div className="col-md-3">
-                        <div className="button btn-group">
-                            <button type="button"
-                                    className={this.state.order === "asc" ? "btn btn-default active" : "btn btn-default"}
-                                    onClick={(e) => this.sort("asc", e)}><i className="fa fa-sort-alpha-asc" aria-hidden="true"/></button>
-                            <button type="button"
-                                    className={this.state.order === "desc" ? "btn btn-default active" : "btn btn-default"}
-                                    onClick={(e) => this.sort("desc", e)}><i className="fa fa-sort-alpha-desc" aria-hidden="true"/></button>
-                        </div>
-                    </div>
-                    <div className="col-md-4">
-                        <label>
-                            <strong>criminal:  </strong>
-
-                        </label><span> </span>
-                        <select value={this.state.selectedCriminal} onChange={this.handlecriminal}>
-                                {criminalMenu}
-                            </select>
-                    </div>
-                    <div className="col-md-4">
-                        <label>
-                            <strong>State:  </strong>
-                        </label><span> </span>
-                        <select value={this.state.selectedState} onChange={this.handleState}>
-                                {stateMenu}
-                            </select>
-                    </div>
-                </div>
+                
                 {/* Break array into separate arrays and wrap each array containing 3 components in a row div */}
                 { chunk(crimeComponents, 4).map(function(row) {
                     return (
@@ -221,12 +143,12 @@ export default class Crimes extends Component {
                         </div>
                     )
                 })}
-                <PageSelector handlePageChange={this.handlePageChange}
+                {/*<PageSelector handlePageChange={this.handlePageChange}
                               handlePrev={this.handlePrev}
                               handleNext={this.handleNext}
                               numPages={this.state.numPages}
                               currentPage={this.state.page}
-                              navigateTo="/Crimes"/>
+                              navigateTo="/Crimes"/>*/}
             </div>
         );
     }

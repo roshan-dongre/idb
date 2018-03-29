5ae949a6addfe26a076138f29cd074be924852af
Zara Louis <zaralouis13@gmail.com>
2018-03-29 17:18:19 -0500

    Added alphabetical sorting to Crimes and States

--- frontend/src/States.js
+++ frontend/src/States.js
@@ -13,6 +13,7 @@ export default class Criminals extends Component {
             numPages: 0,
             totalCount: 0,
             pgSize: 16,
+            sortBy: "",
             pathname: "/States"
         }
         this.apiUrl = 'http://api.ontherun.me:5000/states';
@@ -47,6 +48,12 @@ export default class Criminals extends Component {
         if (this.state.page < this.state.numPages - 1) {
             this.setState({page: this.state.page + 1})
         }
+
+
+    }
+
+    sort = (order) => {
+        this.setState({sortBy: order})
     }
 
     callAPI = () => {
@@ -55,6 +62,10 @@ export default class Criminals extends Component {
         let offset = this.state.page
         let limOff = "?limit="+limit+"&offset="+offset
         let url = "http://api.ontherun.me:5000/states" + limOff
+
+        if (this.state.sortBy !== "") {
+            url += "&sort="+this.state.sortBy
+        }
 
         let self = this
         axios.get(url)
@@ -78,6 +89,11 @@ export default class Criminals extends Component {
      */
 
     componentDidUpdate(prevProps, prevState) {
+
+        if (prevState.sortBy != this.state.sortBy) {
+            this.callAPI()
+        }
+
 
         if (prevState.page !== this.state.page) {
             this.callAPI()
@@ -113,6 +129,49 @@ export default class Criminals extends Component {
 
         return (
             <div className="container sub-container">
+
+
+                <div className="row row-m-b">
+                        <div className="col-md-3">
+                            <div className= "text-left">
+                            <div className="button btn-group">
+                                <button type="button"
+                                      className={this.state.order === "ASC" ? "btn btn-default active" : "btn btn-default"}
+                                      onClick={(e) => this.sort("ASC", e)}><i className="fa fa-sort-alpha-asc" aria-hidden="true"/></button>
+                                <button type="button"
+                                      className={this.state.order === "DESC" ? "btn btn-default active" : "btn btn-default"}
+                                      onClick={(e) => this.sort("DESC", e)}><i className="fa fa-sort-alpha-desc" aria-hidden="true"/></button>
+                            </div>
+                            </div>
+                        </div>
+
+                        {/*
+                        <div className="col-md-3">
+                            <div className = "text-left" style = {blackStyles}>
+                            <label>
+                                <strong style = {whiteStyles}>Gender:  </strong>
+                            </label><span> </span>
+                            <select value={this.state.sex} onChange={this.handleSex}>
+                                    <option value="Unknown"> None </option>
+                                    <option value="Male">Male</option>
+                                    <option value="Female">Female</option>
+                            </select>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <label>
+                                <strong>Style:  </strong>
+                            </label><span> </span>
+                            <select value={this.state.style} onChange={this.handleStyle}>
+                                    {styleMenu}
+                            </select>
+                        </div>
+                        */}
+
+                </div>
+
+
+
                 {/* Break array into separate arrays and wrap each array containing 3 components in a row div */}
                 { chunk(stateComponents, 4).map((row) => {
                     return (
@@ -126,7 +185,7 @@ export default class Criminals extends Component {
                               handleNext={this.handleNext}
                               numPages={this.state.numPages}
                               currentPage={this.state.page}
-                              navigateTo="/Criminals"/>}
+                              navigateTo="/States"/>}
             </div>
       );
     }

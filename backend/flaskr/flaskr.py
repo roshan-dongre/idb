b6448714eee14a4f4b7392d3f88ad59ca935b342
Ricardo Riveron <larius828@yahoo.com>
2018-03-29 15:29:34 -0500

    Added filtering and sort to all models, need more information on criminals, nothing to be actually filtered available just yet

--- backend/flaskr/flaskr.py
+++ backend/flaskr/flaskr.py
@@ -1,90 +1,24 @@
 import os
-import sqlite3
-from flask import Flask, jsonify, request, session, g, redirect, url_for, abort, \
-     render_template, flash
-from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
+from flask import jsonify, request, session, g, redirect, url_for, abort
 from sqlalchemy.sql import select, asc, desc
+from models import State, Criminal, Crime, CrimesState, CrimesCriminal, app, db
 import ast
 import json
 import unicodedata
 import random
 
-app = Flask(__name__) # create the application instance :)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////mydb.db' # load config from this file , flaskr.py
-#app.config['SERVER_NAME'] = 'ontherun.me:5000'
-CORS(app)
-db = SQLAlchemy(app)
+"""" "" "" "" "" "
+"    Globals     "
+"" "" "" "" "" """
 
 statelist = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "DC", "WV", "WI", "WY"]
 statenames = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachussetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "Washington DC", "West Virginia", "Wisconsin", "Wyoming"]
 criminal_loc = ["DC", "TN", "NV", "CA", "OH", "UT", "PA", "DC", "TX", "OR", "FL", "UT", "DC", "DC", "DC", "DC", "OR", "CO", "AL", "VA", "UT", "OK", "FL", "NY", "CA", "NY", "MI", "CA", "NC", "WA", "CA", "DC", "VA", "NJ", "CA", "FL", "NJ", "TX", "SC", "SC", "NY", "NY", "CA", "PA", "CA", "CA", "NY", "HI", "DC", "MD", "TX", "TX", "TX", "GA", "GA", "GA", "NV", "CA", "CA", "CA", "SC", "OR", "OR", "TX", "KS", "KS", "MI", "PA", "PA", "PA", "HI", "CA", "FL", "FL", "CA", "IL", "IL", "WI", "MD", "MD", "NY", "NY", "GA", "NJ", "MD", "GA", "IL", "MD", "SC", "CA", "GA", "CO", "CO", "GA", "MD", "GA", "CA", "CA", "MI", "CA", "OR", "CA", "CA", "NJ", "AL", "CA", "IL", "MI", "CA", "CA", "CA", "PA", "IL", "NJ", "PA", "NY", "MI", "AZ", "AZ", "TX", "PA", "NY", "LA", "AR", "CA", "FL", "FL", "FL", "NY", "PA", "MD", "CA", "NC", "PA", "SC", "NY", "NY", "NY", "NY", "NY", "NJ", "NY", "NE", "NJ", "NY", "NE", "PA", "GA", "DC", "DC", "NY", "OR", "PA", "CA", "SC", "IL", "CA", "KS", "CO", "PA", "PA", "CO", "OR", "GA", "AL", "FL", "MI", "CO", "KS", "IL", "NM", "IL", "TX", "GA", "IL", "OR", "CA", "CA", "CA", "GA", "CA", "CA", "CA", "CO", "CA", "OH", "MD", "LA", "TX", "VA", "CA", "NJ", "DC", "FL", "FL", "FL", "FL", "FL", "FL", "NV", "CA", "NE", "NY", "NY", "FL", "NJ", "CA", "CA", "FL", "MN", "MS", "FL", "CA", "AR", "FL", "OR", "PA", "CA", "PA", "MO", "TN", "AR", "IL", "SC", "CA", "IL", "IL", "IL", "IL", "IL", "OK", "OH", "NJ", "AR", "AR", "NY", "AR", "NM", "NY", "NY", "NY", "NY", "NY", "NY", "NY", "WI", "TX", "TX", "NE", "NE", "NE", "NE", "NY", "CA", "HI", "NJ", "PA", "OR", "NY", "NY", "NY", "MA", "NY", "DC", "NY", "NY", "AL", "OR", "NY", "NY", "FL", "DC", "DC", "DC", "NY", "DC", "NY", "DC", "DC", "FL", "NY", "OR", "NJ", "NJ", "TX", "NJ", "DC", "NY", "NY"]
-class State(db.Model):
-    __tablename__ = 'states'
-    id = db.Column(db.Integer,primary_key=True,unique=True)
-    population = db.Column(db.Integer)
-    abbreviation = db.Column(db.String(10), nullable=False)
-    image = db.Column(db.String(600))
-    name = db.Column(db.String(600))
 
-    def __repr__(self):
-        return "{'name': %r, 'image': %r, 'abbreviation': %r, 'population': %r, 'id': %r}" % (self.name, self.image, self.abbreviation, self.population, self.id)
-
-class Criminal(db.Model):
-    __tablename__ = 'criminals'
-    id = db.Column(db.Integer,primary_key=True,unique=True)
-    name = db.Column(db.String(80), nullable=False)
-    field_office = db.Column(db.String(80), nullable=False)
-    height = db.Column(db.Integer)
-    weight = db.Column(db.Integer)
-    sex = db.Column(db.String(80))
-    hair = db.Column(db.String(80))
-    eyes = db.Column(db.String(80))
-    dob = db.Column(db.String(80))
-    race = db.Column(db.String(80))
-    nationality = db.Column(db.String(80))
-    crime = db.Column(db.String(600), nullable=False)
-    image = db.Column(db.String(600))
-    state = db.Column(db.String(600))
-
-    def __repr__(self):
-        return "{'state': %r, 'image': %r, 'id': %r, 'name': %r, 'field_office': %r, 'height': %r, 'weight': %r, 'sex': %r, 'hair': %r, 'eyes': %r, 'dob': %r, 'race': %r, 'nationality': %r, 'crime': %r}" % (self.state, self.image, self.id, self.name, self.field_office, self.height, self.weight, self.sex, self.hair, self.eyes, self.dob, self.race, self.nationality, self.crime)
-
-
-class Crime(db.Model):
-    __tablename__ = 'crimes'
-    id = db.Column(db.Integer,primary_key=True,unique=True)
-    name = db.Column(db.String(80), nullable=False)
-    image = db.Column(db.String(600))
-    description = db.Column(db.String(6000))
-
-    def __repr__(self):
-        return "{'image': %r, 'id': %r, 'name': %r, 'description': %r}" % (self.image, self.id, self.name, self.description)
-
-class CrimesState(db.Model):
-    __tablename__ = 'crimesTostate'
-    id = db.Column(db.Integer,primary_key=True,unique=True)
-    state_id = db.Column(db.Integer)
-    state_abbreviation = db.Column(db.String(10))
-    state_name = db.Column(db.String(600))
-    crime_id = db.Column(db.Integer)
-    crime_name = db.Column(db.String(600))
-
-    def __repr__(self):
-        return "{'state_name': %r, 'state_abbreviation': %r, 'id': %r, 'state_id': %r, 'crime_id': %r, 'crime_name': %r}" % (self.state_name, self.state_abbreviation, self.id, self.state_id, self.crime_id, self.crime_name)
-
-class CrimesCriminal(db.Model):
-    __tablename__='crimeTocriminal'
-    id = db.Column(db.Integer,primary_key=True,unique=True)
-    crime_id = db.Column(db.Integer)
-    criminal_id = db.Column(db.Integer)
-
-    def __repr__(self):
-        return "{'crime_id': %r, 'id': %r, 'criminal_id': %r}" % (self.crime_id, self.id, self.criminal_id)
-
-
-@app.route('/states', methods=['GET'])#, subdomain="api")
-def get_states():
+"""" "" "" "" "" "
+" Helper Funcs   "
+"" "" "" "" "" """
+def get_lim_off():
     limit = request.args.get('limit','')
     if limit == '':
         limit = 289
@@ -92,7 +26,33 @@ def get_states():
     if offset == '':
         offset = 0
     offset = int(offset)*int(limit)
-    return jsonify({'totalCount': db.session.query(State).count(), 'states': ast.literal_eval(str(State.query.filter(State.id>offset).limit(limit).all()))})
+    return {'lim': limit,'off': offset}
+
+"""" "" "" "" "" "
+" API Endpoints  "
+"" "" "" "" "" """
+
+@app.route('/states', methods=['GET'])#, subdomain="api")
+def get_states():
+    page_res = get_lim_off()
+    filt_pop = request.args.get('population','')
+    sort_name = request.args.get('sort','')
+    my_query = {}
+    if filt_pop == '':
+        my_query['totalCount'] = db.session.query(State).count()
+        if sort_name == "ASC":
+            my_query['states'] = ast.literal_eval(str(State.query.order_by(asc(State.name)).offset(page_res['off']).limit(page_res['lim']).all()))
+        if sort_name == "DESC":
+            my_query['states'] = ast.literal_eval(str(State.query.order_by(desc(State.name)).offset(page_res['off']).limit(page_res['lim']).all()))
+    else:
+        my_query['totalCount'] = db.session.query(State).filter(State.population>=int(filt_pop)).count()
+        if sort_name == "ASC":
+            my_query['states'] = ast.literal_eval(str(State.query.filter(State.population>=int(filt_pop)).order_by(asc(State.name)).offset(page_res['off']).limit(page_res['lim']).all()))
+        elif sort_name == "DESC":
+            my_query['states'] = ast.literal_eval(str(State.query.filter(State.population>=int(filt_pop)).order_by(desc(State.name)).offset(page_res['off']).limit(page_res['lim']).all()))
+        else:
+            my_query['states'] = ast.literal_eval(str(State.query.filter(State.population>=int(filt_pop)).offset(page_res['off']).limit(page_res['lim']).all()))
+    return jsonify(my_query)
 
 @app.route('/states/<string:state_name>', methods=['GET'])#, subdomain="api")
 def get_state(state_name):
@@ -105,26 +65,22 @@ def get_state(state_name):
 
 @app.route('/criminals', methods=['GET'])#, subdomain="api")
 def get_criminals():
-    limit = request.args.get('limit','')
-    if limit == '':
-        limit = 289
-    offset = request.args.get('offset','')
-    if offset == '':
-        offset = 0
-    offset = int(offset)*int(limit)
+    page_res = get_lim_off()
     gender = request.args.get('sex','')
     sort_name = request.args.get('sort','')
     if gender == '':
+        totalCount = db.session.query(Criminal).count()
         if sort_name == "ASC":
-            return jsonify({'totalCount': db.session.query(Criminal).count(), 'criminals': ast.literal_eval(str(Criminal.query.order_by(asc(Criminal.name)).offset(offset).limit(limit).all()))})
+            return jsonify({'totalCount': totalCount, 'criminals': ast.literal_eval(str(Criminal.query.order_by(asc(Criminal.name)).offset(page_res['off']).limit(page_res['lim']).all()))})
         if sort_name == "DESC":
-            return jsonify({'totalCount': db.session.query(Criminal).count(), 'criminals': ast.literal_eval(str(Criminal.query.order_by(desc(Criminal.name)).offset(offset).limit(limit).all()))})
-        return jsonify({'totalCount': db.session.query(Criminal).count(), 'criminals': ast.literal_eval(str(Criminal.query.filter(Criminal.id>offset).limit(limit).all()))})
+            return jsonify({'totalCount': totalCount, 'criminals': ast.literal_eval(str(Criminal.query.order_by(desc(Criminal.name)).offset(page_res['off']).limit(page_res['lim']).all()))})
+        return jsonify({'totalCount': totalCount, 'criminals': ast.literal_eval(str(Criminal.query.filter(Criminal.id>page_res['off']).limit(page_res['lim']).all()))})
+    totalCount = db.session.query(Criminal).filter_by(sex=gender).count()
     if sort_name == "ASC":
-        return jsonify({'totalCount': db.session.query(Criminal).filter_by(sex=gender).count(), 'criminals': ast.literal_eval(str(Criminal.query.filter_by(sex=gender).order_by(asc(Criminal.name)).offset(offset).limit(limit).all()))})
+        return jsonify({'totalCount': totalCount, 'criminals': ast.literal_eval(str(Criminal.query.filter_by(sex=gender).order_by(asc(Criminal.name)).offset(page_res['off']).limit(page_res['lim']).all()))})
     if sort_name == "DESC":
-        return jsonify({'totalCount': db.session.query(Criminal).filter_by(sex=gender).count(), 'criminals': ast.literal_eval(str(Criminal.query.filter_by(sex=gender).order_by(desc(Criminal.name)).offset(offset).limit(limit).all()))})
-    return jsonify({'totalCount': db.session.query(Criminal).filter_by(sex=gender).count(), 'criminals': ast.literal_eval(str(Criminal.query.filter(Criminal.id>offset).filter_by(sex=gender).limit(limit).all()))})
+        return jsonify({'totalCount': totalCount, 'criminals': ast.literal_eval(str(Criminal.query.filter_by(sex=gender).order_by(desc(Criminal.name)).offset(page_res['off']).limit(page_res['lim']).all()))})
+    return jsonify({'totalCount': totalCount, 'criminals': ast.literal_eval(str(Criminal.query.filter(Criminal.id>page_res['off']).filter_by(sex=gender).limit(page_res['lim']).all()))})
 
 @app.route('/criminals/<int:crim_id>', methods=['GET'])#, subdomain="api")
 def get_criminal(crim_id):
@@ -132,14 +88,26 @@ def get_criminal(crim_id):
 
 @app.route('/crimes', methods=['GET'])#, subdomain="api")
 def get_crimes():
-    limit = request.args.get('limit','')
-    if limit == '':
-        limit = 289
-    offset = request.args.get('offset','')
-    if offset == '':
-        offset = 0
-    offset = int(offset)*int(limit)
-    return jsonify({'totalCount': db.session.query(Crime).count(), 'crimes': ast.literal_eval(str(Crime.query.filter(Crime.id>offset).limit(limit).all()))})
+    page_res = get_lim_off()
+    filt_pop = request.args.get('population','')
+    sort_name = request.args.get('sort','')
+    my_query = {}
+    if filt_pop == '':
+        my_query['totalCount'] = db.session.query(Crime).count()
+        if sort_name == "ASC":
+            my_query['states'] = ast.literal_eval(str(Crime.query.order_by(asc(Crime.name)).offset(page_res['off']).limit(page_res['lim']).all()))
+        if sort_name == "DESC":
+            my_query['states'] = ast.literal_eval(str(Crime.query.order_by(desc(Crime.name)).offset(page_res['off']).limit(page_res['lim']).all()))
+    else:
+        my_query['totalCount'] = db.session.query(Crime).filter(Crime.population>=int(filt_pop)).count()
+        if sort_name == "ASC":
+            my_query['states'] = ast.literal_eval(str(Crime.query.filter(Crime.population>=int(filt_pop)).order_by(asc(Crime.name)).offset(page_res['off']).limit(page_res['lim']).all()))
+        elif sort_name == "DESC":
+            my_query['states'] = ast.literal_eval(str(Crime.query.filter(Crime.population>=int(filt_pop)).order_by(desc(Crime.name)).offset(page_res['off']).limit(page_res['lim']).all()))
+        else:
+            my_query['states'] = ast.literal_eval(str(Crime.query.filter(Crime.population>=int(filt_pop)).offset(page_res['off']).limit(page_res['lim']).all()))
+    return jsonify(my_query)
+    # return jsonify({'totalCount': db.session.query(Crime).count(), 'crimes': ast.literal_eval(str(Crime.query.filter(Crime.id>offset).limit(limit).all()))})
 
 @app.route('/crimes/state/<int:stat_id>', methods=['GET'])#, subdomain="api")
 def get_crimesperstate(stat_id):
@@ -184,28 +152,16 @@ def get_crimetocriminals(catch_id):
 def get_criminalstocrime(catch_id):
     return jsonify(ast.literal_eval(str(CrimesCriminal.query.filter_by(criminal_id=catch_id).all())))
 
-
-# /api/states/                          //done
-# /api/states/<abr>                     //done
-# /api/states/crimes                    
-# /api/states/crimes/<id>               
-# /api/states/criminals                 
-# /api/states/criminals/<id>            
-# /api/criminals/                       //done
-# /api/criminals/<id>                   //done
-# /api/crimes/                          //done
-# /api/crimes/criminals
-# /api/crimes/criminals/<crime_id>
-
 #404 handling for api 
 @app.errorhandler(404)
 def pageNotFound(error):
     return redirect("https://roshan-dongre.gitbooks.io/api/")
 
+"""" "" "" "" "" "
+" Database Init  "
+"" "" "" "" "" """
 
-# start the server with the 'run()' method
 if __name__ == '__main__':
-#    initdb_command()
     db.reflect()
     db.drop_all()
     db.create_all()

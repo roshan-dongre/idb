c2d899dcac018176f2f4456b4fbab9970a961ed7
Ricardo Riveron <larius828@yahoo.com>
2018-04-18 22:58:17 -0500

    Cleaned up unnecessary code

--- backend/flaskr/__init__.py
+++
@@ -1,8 +0,0 @@
-from .flaskr import app
-
-@app.route('/')
-def show_entries():
-    db = get_db()
-    cur = db.execute('select title, text from entries order by id desc')
-    entries = cur.fetchall()
-    return render_template('show_entries.html', entries=entries)

aeac694d0f4633c3411ae740387669be795f4e60
Ricardo Riveron <larius828@yahoo.com>
2018-04-25 21:50:45 -0500

    Added more data for visualization

--- frontend/src/canitstream/getData.py
+++ frontend/src/canitstream/getData.py
@@ -14,47 +14,33 @@ def write_data(result,outfile):
     success = False
     country_name = result["name"].encode('ascii', 'ignore')
     country_id = result["id"]
+    country_lat = result["latitude"]
+    country_lng = result["longitude"]
+    country_pop = result["population"]
     country_streams = [{} for i in range(5)]
     country_movies = [{} for i in range(5)]
 
     url = URL+"/"+str(country_id)+"/streaming"
     response = requests.get(url)
     data = json.loads(response.content)
-    i = 0
     for instance in data["data"]:
-    	if i == 5:
-    		break
     	country_streams[i] = "{\"name\": \""+instance["name"]+"\"}"
-    	i += 1
     mystream = ""
-    first = 1
     for stream in country_streams:
     	if len(stream)>0:
-    		if first == 1:
-    			first = 0
-    		else:
-    			mystream+=","
-    		mystream += stream
-
+            mystream += stream
+            break
     url = URL+"/"+str(country_id)+"/movie"
     response = requests.get(url)
     data = json.loads(response.content)
-    i = 0
     for instance in data["data"]:
-    	if i == 5:
-    		break
     	country_movies[i] = "{\"name\": \""+instance["name"]+"\"}"
-    	i += 1
     mymovie = ""
-    first = 1 
     for stream in country_movies:
     	if len(stream)>0:
-    		if first == 1:
-    			first = 0
-    		else:
-    			mymovie+=","
-    		mymovie += stream.encode('ascii', 'ignore')
-    outfile.write("{\"country\": \""+country_name+"\", \"streams\": ["+mystream+"], \"movies\": ["+mymovie+"]},")
+            mymovie += stream.encode('ascii', 'ignore')
+            break
+    outfile.write("{\"population\": \""+country_pop+"{\"longitude\": \""+country_lng+"{\"latitude\": \""+country_lat+"\"country\": \""+country_name+"\", \"streams\": ["+mystream+"], \"movies\": ["+mymovie+"]},")
     success = True
     return success
 
